7099ab51533adbdd6a56234638fc09ed8ac29fb4
Volodymyr Panchenko <panillich@gmail.com>
2026-04-14 09:03:55 +0200

    Added 14.04

--- src/test/java/demowebshop/User.java
+++ src/test/java/demowebshop/User.java
@@ -1,10 +1,10 @@
 package demowebshop;
 
 public class User {
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
 
     public User(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
